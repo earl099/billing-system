8a9c7c8bbb0cb4ae6363a0adffcb3dc2ec0e9aa5
Earl <earlsaturay09@gmail.com>
2026-01-14 17:07:25 +0800

    Test Commit 40
    - Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,9 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
-app.options('*', cors())
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', ALL_ROUTES)
 
